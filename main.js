8fe2c34efc5216ac7b000778c77c5c055101c26e
dave-santiano <dss441@nyu.edu>
2019-03-01 16:44:13 +0800

    added rainbow effect

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
-var mushroom = document.getElementById("mushroom");
 var mushroomVIPList = [];
-
 
 function Mushroom(x, y){
     this.x = x;
     this.y = y;
-    this.size = Math.random() * 200 + 20;
+    this.maxSize = 200;
+    this.size = Math.random() * this.maxSize + 20;
+
     this.img = document.createElement("IMG");
     this.img.style.top = "-200px";
     this.img.style.left = "-200px";
@@ -14,11 +14,14 @@ function Mushroom(x, y){
     this.img.style.width = this.size + "px";
     this.img.style.display = "block";
     this.img.style.position = "absolute";
-    this.img.style.zIndex = Math.floor(this.size) + ""; 
-    // console.log(this.img.style.zIndex);
-    this.speed = this.size / 200;
+    this.img.style.zIndex = Math.floor(this.size) + "";
+    this.hueValue = (this.size / this.maxSize) * 360;
+    this.img.style.filter = "hue-rotate(" + this.hueValue + "deg)";
+    // console.log(this.img.style.filter);
+    this.speed = this.size / this.maxSize;
     this.speedModifierX = 3;
     this.speedModifierY = 2;
+
     document.body.append(this.img);
 
     this.spawn = function(){
@@ -42,15 +45,11 @@ function spawnMushroom(){
 function draw(){
     for(var i = 0; i < mushroomVIPList.length; i++){
         mushroomVIPList[i].move();
-        // console.log(Math.floor(mushroomVIPList[i].zIndex));
     }
 }
 
 function setup(){
     setInterval(function(){draw();}, 16);
 }
-
-
-
 
 setup();
